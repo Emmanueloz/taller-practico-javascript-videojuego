4a9556799af813983b2f55ccfc6094a5ed69cd3c
Emmanueloz <davidozuna84@hotmail.com>
2022-09-10 12:59:35 -0500

    Canvas responsive

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
-window.addEventListener("load", startGame);
+let canvasSize;
+let elementsSize;
 
-function startGame() {
-  let canvasSize;
+window.addEventListener("load", setCanvasSize);//cuando termine de cargar
+window.addEventListener("resize", setCanvasSize);// nuevo tamaño de pantalla
 
+function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
     canvasSize = window.innerWidth * 0.8;
   } else {
@@ -15,8 +17,12 @@ function startGame() {
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
 
-  const elementsSize = canvasSize / 10;
+  elementsSize = canvasSize / 10;
 
+  startGame();
+}
+
+function startGame() {
   console.log({ canvasSize, elementsSize });
 
   game.font = elementsSize + "px Verdana";
@@ -25,14 +31,4 @@ function startGame() {
   for (let i = 1; i <= 10; i++) {
     game.fillText(emojis["X"], elementsSize, elementsSize * i);
   }
-
-  /* game.fillRect(0, 50, 100, 100);// crea una figura rectangular los dos primeros datos es el inicio y el segundo es su final (x,y,x,y)
-  game.clearRect(50, 50, 50, 50);// Crea un rectángulo como borrador se usa los mismos datos que al crear un rectángulo 
-  game.clearRect();
-  game.clearRect(0, 0, 50, 50); */
-
-  /* game.font = '25px Verdana'//Estilos de fuentes tipográficas
-  game.fillStyle = 'purple'//Estilos que se aplica
-  game.textAlign = 'start'//Alineación del texto , se especifica si las coordenadas que se le de se usara donde inicia el texto o donde termina
-  game.fillText('David', 25, 25)//Coloca un texto, y se coloca las coordenadas donde se inicia el texto */
 }
