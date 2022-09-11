f0aeadee86a79dc69db67b2e338027bf1a11f1b8
Emmanueloz <davidozuna84@hotmail.com>
2022-09-11 18:59:07 -0500

    Limpieza de movimientos

--- game.js
+++ game.js
@@ -27,12 +27,14 @@ function setCanvasSize() {
   canvas.setAttribute("height", canvasSize);
 
   elementsSize = canvasSize / 10;
-
+  // Reset position player  
+  playerPosition.x = undefined
+  playerPosition.y = undefined
   startGame();
 }
 
 function startGame() {
-  console.log({ canvasSize, elementsSize });
+  //console.log({ canvasSize, elementsSize });
 
   game.font = `${elementsSize}px Verdana`; //Tamaño del emoji del tamaño del cuadrado
   game.textAlign = "end"; //Alineación del emoji
@@ -44,9 +46,10 @@ function startGame() {
   quitar espacios, split es para crear un array a partir de 
   un string dividiendo por algún carácter en este casa 
   el "\n" que es un salto de linea */
-  console.log(mapRows, mapRowCols);
+  //console.log(mapRows, mapRowCols);
 
   //Recorremos el array para dibujarlo en el canvas
+  game.clearRect(0, 0, canvasSize, canvasSize);
   mapRowCols.forEach((row, rowI) => {
     //primer forEach nos da el array de cada fila
     row.forEach((col, colI) => {
@@ -55,9 +58,12 @@ function startGame() {
       const posX = elementsSize * (colI + 1); //Posición en x
       const posY = elementsSize * (rowI + 1); //Posición en y
 
-      if (playerPosition.x == undefined &&col == "O") {
-        playerPosition.x = posX;
-        playerPosition.y = posY;
+      if (playerPosition.x == undefined && col == "O") {
+        if (!playerPosition.x == !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+          console.log(playerPosition, elementsSize);
+        }
       }
 
       game.fillText(emoji, posX, posY);
@@ -65,7 +71,7 @@ function startGame() {
     });
   });
 
-  movePlayer()
+  movePlayer();
 }
 
 function movePlayer() {
@@ -86,19 +92,46 @@ function moveByKeys(event) {
 }
 
 function moveUp() {
-  playerPosition.y -= elementsSize
-  movePlayer()
-  setCanvasSize()
+  if (playerPosition.y <= elementsSize + 1) {
+    console.log("no se mueve", playerPosition.y, elementsSize);
+  } else {
+    console.log("se mueve", playerPosition.y, elementsSize);
+    playerPosition.y -= elementsSize;
+    startGame();
+  }
+  console.log("Arriba");
 }
 
 function moveLeft() {
-  console.log("Izquierda");
+  if (playerPosition.x <= elementsSize + 1) {
+    console.log("no se mueve",);
+  } else{
+    console.log("se mueve");
+    playerPosition.x -= elementsSize;
+    startGame();
+  }
+  console.log("Izquierda", playerPosition.x, elementsSize, canvasSize);
 }
 
 function moveRight() {
-  console.log("Derecha");
+  if (playerPosition.x >= canvasSize) {
+    console.log("no se mueve");
+  } else {
+    console.log("se mueve");
+    playerPosition.x += elementsSize;
+    startGame();
+  }
+
+  console.log("Derecha",playerPosition.x,elementsSize, canvasSize);
 }
 
 function moveDown() {
+  if (playerPosition.y >= canvasSize) {
+    console.log("no se mueve", playerPosition.y, canvasSize);
+  } else {
+    console.log("se mueve", playerPosition.y, canvasSize);
+    playerPosition.y += elementsSize;
+    startGame();
+  }
   console.log("Abajo");
 }
