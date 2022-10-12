efbffa623ad7f3e53b7ea5613e964f0f2c19a3d2
Emmanueloz <davidozuna84@hotmail.com>
2022-10-11 20:29:39 -0500

    Derrota: perdiendo vidas

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
-const btnUP = document.querySelector("#up");
+const btnUp = document.querySelector("#up");
 const btnLeft = document.querySelector("#left");
 const btnRight = document.querySelector("#right");
 const btnDown = document.querySelector("#down");
+//const pVidas = document.getElementById("vidas");
 
 let canvasSize;
 let elementsSize;
 let level = 0;
-//let gameOverBool = false;
+let lives = 3;
+//pVidas.innerHTML = lives;
 
 const playerPosition = {
   x: undefined,
@@ -18,11 +20,10 @@ const giftPosition = {
   x: undefined,
   y: undefined,
 };
-
 let enemyPositions = [];
 
-window.addEventListener("load", setCanvasSize); //cuando termine de cargar
-window.addEventListener("resize", setCanvasSize); // nuevo tamaño de pantalla
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
@@ -35,49 +36,41 @@ function setCanvasSize() {
   canvas.setAttribute("height", canvasSize);
 
   elementsSize = canvasSize / 10;
-  // Reset position player
-  playerPosition.x = undefined;
-  playerPosition.y = undefined;
+
   startGame();
 }
 
 function startGame() {
-  //console.log({ canvasSize, elementsSize });
+  console.log({ canvasSize, elementsSize });
 
-  game.font = `${elementsSize}px Verdana`; //Tamaño del emoji del tamaño del cuadrado
-  game.textAlign = "end"; //Alineación del emoji
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
 
   const map = maps[level];
-  console.log(level); //Mapa seleccionado
+
   if (!map) {
-    game.fillText(emojis["WIN"], playerPosition.x, playerPosition.y);
     gameWin();
     return;
   }
-  const mapRows = map.trim().split("\n"); //Filas del Mapa
-  const mapRowCols = mapRows.map((row) => row.trim().split("")); //Array de arrays fila, elemento de la fila
-  /* .trim es una función que funciona en stings para
-  quitar espacios, split es para crear un array a partir de 
-  un string dividiendo por algún carácter en este casa 
-  el "\n" que es un salto de linea */
-  //console.log(mapRows, mapRowCols);
 
-  //Recorremos el array para dibujarlo en el canvas
+  const mapRows = map.trim().split("\n");
+  const mapRowCols = mapRows.map((row) => row.trim().split(""));
+  console.log({ map, mapRows, mapRowCols });
+
   enemyPositions = [];
   game.clearRect(0, 0, canvasSize, canvasSize);
-  mapRowCols.forEach((row, rowI) => {
-    //primer forEach nos da el array de cada fila
-    row.forEach((col, colI) => {
-      //El segundo for Each nos da cada elemento dentro de la fila que usamos para guardarla en una variable
-      const emoji = emojis[col];
-      const posX = elementsSize * (colI + 1); //Posición en x
-      const posY = elementsSize * (rowI + 1); //Posición en y
 
-      if (playerPosition.x == undefined && col == "O") {
-        if (!playerPosition.x == !playerPosition.y) {
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+
+      if (col == "O") {
+        if (!playerPosition.x && !playerPosition.y) {
           playerPosition.x = posX;
           playerPosition.y = posY;
-          console.log(playerPosition, elementsSize);
+          console.log({ playerPosition });
         }
       } else if (col == "I") {
         giftPosition.x = posX;
@@ -90,9 +83,9 @@ function startGame() {
       }
 
       game.fillText(emoji, posX, posY);
-      //console.log({row, colI, col,rowI});
     });
   });
+
   movePlayer();
 }
 
@@ -106,23 +99,18 @@ function movePlayer() {
   if (giftCollision) {
     levelWin();
   }
-  /* El método .find de los arrays lo que devuelve el elemento que cumpla cierta condición, también
-  Podemos devolver un true o false si retornamosEl método .find de los arrays lo que devuelve el elemento que cumpla cierta condición, también
-  Podemos devolver un true o false si retornamos una validación una validación */
+
   const enemyCollision = enemyPositions.find((enemy) => {
     const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
     const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
-
     return enemyCollisionX && enemyCollisionY;
   });
 
   if (enemyCollision) {
-    gameOver();
-  } else if (giftCollision && !maps[level]) {
-    console.log("Funciona");
-  }else {
-    game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+    levelFail();
   }
+
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
 }
 
 function levelWin() {
@@ -131,32 +119,27 @@ function levelWin() {
   startGame();
 }
 
-function gameWin() {
-  console.log("Terminaste el juego");
-  game.fillText(emojis["WIN"], playerPosition.x, playerPosition.y);
-  setTimeout(() => {
-    alert("Ganaste el juego, presione  Aceptar para reiniciar");
-    playerPosition.x = undefined;
-    playerPosition.y = undefined;
+function levelFail() {
+  --lives;
+  if (lives == 0) {
     level = 0;
-    startGame();
-  }, 200);
+    lives = 3;
+    //pVidas.innerHTML = lives;
+  }
+  console.log("Chocaste contra un enemigo :(");
+  playerPosition.x = undefined;
+  playerPosition.y = undefined;
+  //pVidas.innerHTML = lives;
+  console.log("Vidas: ", lives);
+  startGame();
 }
 
-function gameOver() {
-  console.log("Moriste :(");
-  game.fillText(emojis["BOMB_COLLISION"], playerPosition.x, playerPosition.y);
-  setTimeout(() => {
-    alert("Moriste, presiona Aceptar para reiniciar");
-    playerPosition.x = undefined;
-    playerPosition.y = undefined;
-    level = 0;
-    startGame();
-  }, 100);
+function gameWin() {
+  console.log("¡Terminaste el juego!");
 }
 
 window.addEventListener("keydown", moveByKeys);
-btnUP.addEventListener("click", moveUp);
+btnUp.addEventListener("click", moveUp);
 btnLeft.addEventListener("click", moveLeft);
 btnRight.addEventListener("click", moveRight);
 btnDown.addEventListener("click", moveDown);
@@ -167,36 +150,40 @@ function moveByKeys(event) {
   else if (event.key == "ArrowRight") moveRight();
   else if (event.key == "ArrowDown") moveDown();
 }
-//Funciones de movimiento
 function moveUp() {
-  if (playerPosition.y - elementsSize + 1 < elementsSize) {
+  console.log("Me quiero mover hacia arriba");
+
+  if (playerPosition.y - elementsSize < elementsSize) {
     console.log("OUT");
   } else {
     playerPosition.y -= elementsSize;
     startGame();
   }
 }
-
 function moveLeft() {
-  if (playerPosition.x - elementsSize + 1 < elementsSize) {
+  console.log("Me quiero mover hacia izquierda");
+
+  if (playerPosition.x - elementsSize < elementsSize) {
     console.log("OUT");
   } else {
     playerPosition.x -= elementsSize;
     startGame();
   }
 }
-
 function moveRight() {
-  if (playerPosition.x + elementsSize - 1 > canvasSize) {
+  console.log("Me quiero mover hacia derecha");
+
+  if (playerPosition.x + elementsSize > canvasSize) {
     console.log("OUT");
   } else {
     playerPosition.x += elementsSize;
     startGame();
   }
 }
-
 function moveDown() {
-  if (playerPosition.y + elementsSize - 1 > canvasSize) {
+  console.log("Me quiero mover hacia abajo");
+
+  if (playerPosition.y + elementsSize > canvasSize) {
     console.log("OUT");
   } else {
     playerPosition.y += elementsSize;
